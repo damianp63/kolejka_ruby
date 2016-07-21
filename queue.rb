38eca2e8f44a1692da05e009013747d2738b51bb
damianp63 <damianpodrazka@wp.pl>
2016-07-21 15:35:25 +0200

    pierwsze testy działania metod

--- queue.rb
+++ queue.rb
@@ -1,6 +1,8 @@
+require 'rspec/autorun'
+
 class Queue
-  def initialize
-    $table=[]
+  def initialize(tab)
+    $table=tab
   end
 
   def push(object)
@@ -9,19 +11,44 @@ class Queue
 
   def pop
     $table.delete($table[0])
-    print $table
   end
 
   def size
     $table.size
   end
-
+  def to_s
+   $table
+  end
 end
 
-obj=Queue.new
-obj.push(3)
-obj.push(2)
-obj.push(57)
-puts obj.size
-obj.pop
-puts obj.size
+describe Queue do
+  describe "#push" do
+    it "dodaje element na koniec kolejki" do
+      obj=Queue.new([])
+      obj.push(5)
+      expect(obj.to_s).to eq([5])
+      obj.push(4)
+      obj.push(1233)
+      expect(obj.to_s).to eq([5,4,1233])
+    end
+  end
+  describe "#pop" do
+    it "usuwa pierwszy element z kolejki" do
+      obj=Queue.new([2])
+      obj.push(54)
+      obj.pop
+      expect(obj.to_s).to eq([54])
+    end
+  end
+  describe "#size" do
+    it "podaje rozmiar kolejki" do
+      obj=Queue.new([2])
+      obj.push(54)
+      expect(obj.size).to eq(2)
+      obj.push(8475)
+      expect(obj.size).to eq(3)
+      obj.pop
+      expect(obj.size).to eq(2)
+    end
+  end
+end
